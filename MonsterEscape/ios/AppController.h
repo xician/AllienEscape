259ee4b85f4e9bcc4e74ef4c9b0045917cb015f0
macbook <macbook@macbooks-MacBook-Pro.local>
2013-10-11 15:25:30 +0500

    Initial Commit

---
+++ MonsterEscape/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  MonsterEscapeAppController.h
+//  MonsterEscape
+//
+//  Created by macbook on 10/11/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ MonsterEscape/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  MonsterEscapeAppController.h
+//  MonsterEscape
+//
+//  Created by macbook on 10/11/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
